88c171e625e77d11347752a3f3e4166c37340048
ronnmeru <naoyuki.kojima.0417@gmail.com>
2021-12-01 10:27:28 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,7 @@ Rails.application.routes.draw do
   end
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
+  get   'inquiry'         => 'inquiry#index'     # 入力画面
+  post  'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post  'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
 end
