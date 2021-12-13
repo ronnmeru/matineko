11d07ee0fe5eb9eb081cd98a3ebb97016ed6ec90
ronnmeru <naoyuki.kojima.0417@gmail.com>
2021-12-13 11:56:11 +0000

    add

--- db/migrate/20211029074111_create_posts.rb
+++ db/migrate/20211029074111_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.integer :user_id  , null: false, default: ""
+      t.integer :user_id  
       t.string :title     , null: false, default: ""
-      t.text :content     , null: false, default: ""
+      t.text :content     
 
       t.timestamps
     end
